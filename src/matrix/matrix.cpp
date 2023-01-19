58d90864ef728cb827df87589ed9ba21d9801370
Ramin Mojab <rmojab63@gmail.com>
2023-01-19 09:48:29 +0330

    Fix license in the files

--- src/matrix/matrix.cpp
+++ src/matrix/matrix.cpp
@@ -1,7 +1,7 @@
 
 /*
  Copyright (C) 2022-2023 Ramin Mojab
- Licensed under the LGPL 3.0.
+ Licensed under the GPL 3.0.
  See accompanying file LICENSE
 */
 
--- test/statistics_t.cpp
+++ test/statistics_t.cpp
@@ -1,6 +1,6 @@
 /*
  Copyright (C) 2022-2023 Ramin Mojab
- Licensed under the LGPL 3.0.
+ Licensed under the GPL 3.0.
  See accompanying file LICENSE
 */
 
