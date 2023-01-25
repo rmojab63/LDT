bcdffaf8e775accd35a0be3621480b224a15a099
Ramin Mojab <rmojab63@gmail.com>
2023-01-26 00:13:53 +0330

    Add partial AUC

--- src/ldt_statistics/scoring/roc.cpp
+++ src/ldt_statistics/scoring/roc.cpp
@@ -16,7 +16,14 @@ template <bool hasWeight, bool isBinary> ROC<hasWeight, isBinary>::ROC(Ti n){};
 template <bool hasWeight, bool isBinary>
 void ROC<hasWeight, isBinary>::Calculate(Matrix<Tv> &y, Matrix<Tv> &scores,
                                          Matrix<Tv> *weights,
-                                         bool normalizePoints) {
+                                         bool normalizePoints,
+                                         Tv lowerThreshold, Tv upperThreshold) {
+  bool isPartial = std::isnan(lowerThreshold) == false &&
+                   std::isnan(upperThreshold) == false;
+  if (isPartial)
+    normalizePoints =
+        true; // we should normalize the points before using thresholds
+
   Ti n = y.length();
   if (n == 0)
     throw std::logic_error("zero number of observations in calculating ROC.");
@@ -71,8 +78,38 @@ void ROC<hasWeight, isBinary>::Calculate(Matrix<Tv> &y, Matrix<Tv> &scores,
         std::get<0>(p) /= sumFP;
         std::get<1>(p) /= sumTP;
       }
-      AucPoints<false> a(Points, 0);
-      Result = a.Result; // normalize
+      if (isPartial) { // TODO: create a Bounded AUC class and move this logic
+                       // to that class
+        // throw std::logic_error("Partial AUC is not implemented.");
+        std::vector<std::tuple<Tv, Tv>> newPoints;
+        Tv x, y, x0 = 0, y0 = 0, slope;
+        for (auto &p : Points) {
+          x = std::get<0>(p);
+          y = std::get<1>(p);
+          slope = (y - y0) / (x - x0);
+
+          if (x >= lowerThreshold &&
+              x0 < lowerThreshold) // we should add the first point
+            newPoints.push_back(std::make_tuple(
+                lowerThreshold, y0 + (lowerThreshold - x0) * slope));
+
+          if (x > upperThreshold &&
+              x0 <= upperThreshold) // we should add the last point
+            newPoints.push_back(std::make_tuple(
+                upperThreshold, y - (x - upperThreshold) * slope));
+
+          else if (x >= lowerThreshold && x0 >= lowerThreshold) {
+            newPoints.push_back(std::make_tuple(x, y));
+          }
+          x0 = x;
+          y0 = y;
+        }
+        AucPoints<false> a(newPoints, 0);
+        Result = a.Result;
+      } else {
+        AucPoints<false> a(Points, 0);
+        Result = a.Result;
+      }
     } else {
       AucPoints<false> a(Points, 0);
       Result = a.Result / (sumFP * sumTP); // normalize
